b50c2f4a28abfdcd7975fa1863b46ac953c4a6e9
Hans Windisch <hans-michael.windisch@thi.de>
2026-04-14 18:10:51 +0200

    payments Übung ergänzt

---
+++ kapitel3/payments/processors/PaymentProcessor.java
@@ -0,0 +1,19 @@
+package kapitel3.payments.processors;
+
+public class PaymentProcessor {
+
+	protected final String ownerName;
+
+	public PaymentProcessor(String ownerName) {
+	}
+
+	public abstract double getTransactionFee(double amount);
+
+	public final void process(double amount) {
+		// TODO
+	}
+
+	public void printReceipt(double amount) {
+		// TODO
+	}
+}
