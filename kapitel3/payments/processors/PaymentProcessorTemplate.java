341ce7ebab0562f10db7a6a68e991115151dd12e
Hans Windisch <hans-michael.windisch@thi.de>
2026-04-16 16:22:55 +0200

    lecture 2026-04-16

---
+++ kapitel3/payments/processors/PaymentProcessorTemplate.java
@@ -0,0 +1,19 @@
+package kapitel3.payments.processors;
+
+public class PaymentProcessorTemplate {
+
+	//protected final String ownerName;
+
+	public PaymentProcessorTemplate(String ownerName) {
+	}
+
+	//public abstract double getTransactionFee(double amount);
+
+	public final void process(double amount) {
+		// TODO
+	}
+
+	public void printReceipt(double amount) {
+		// TODO
+	}
+}
